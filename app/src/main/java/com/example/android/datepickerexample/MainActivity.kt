e6976417b7377871a8da025cb8201af2a5204dcb
GabyCN <Gabynoestaquieta@gmail.com>
2021-03-25 14:30:55 +0100

    Funciona

--- app/src/main/java/com/example/android/datepickerexample/MainActivity.kt
+++ app/src/main/java/com/example/android/datepickerexample/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.android.datepickerexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.YearMonth
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etDate.setOnClickListener { showDatePickerDialog() }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day,month,year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    private fun onDateSelected (day:Int, month: Int, year: Int) {
+        etDate.setText("Has seleccionado el $day del $month del año $year")
     }
 }
